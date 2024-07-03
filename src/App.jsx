8c2a7cac68ed0a92480e76159028309121c08eaa
Artur <agavrik464@gmail.com>
2024-07-03 10:40:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Profile from './components/Profile/Profile'
 import FriendsList from './components/FriendList/FriendsList'
 import Transaction from './components/Transaction/Transaction';
 
+import friends from "../../friends.json";
+import transactions from "../../transactions.json";
+
 function App() {
 
   return (
     <>
       <Profile username={userData.username} tag={userData.tag} location={userData.location} avatar={userData.avatar} stats={userData.stats} />
-      <FriendsList />
-      <Transaction />
+      <FriendsList friends={friends} />
+      <Transaction transactions={transactions} />
     </>
   )
 }
