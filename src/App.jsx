b0199b6314f42a327bc93f941f368591a2e5b9e9
Artur <agavrik464@gmail.com>
2024-07-03 10:11:25 +0300

    tasks done

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import './App.css'
 import userData from "./userData.json"
 import Profile from './components/Profile/Profile'
 import FriendsList from './components/FriendList/FriendsList'
-import friends from "./friends.json";
+import Transaction from './components/Transaction/Transaction';
 
 function App() {
 
   return (
     <>
       <Profile username={userData.username} tag={userData.tag} location={userData.location} avatar={userData.avatar} stats={userData.stats} />
-      <hr />
       <FriendsList />
+      <Transaction />
     </>
   )
 }
---
+++ src/components/Transaction/Transaction.jsx
@@ -0,0 +1,38 @@
+import css from "./Transaction.module.css";
+import items from "../../transactions.json";
+
+const Transaction = () => {
+    return (
+        <>
+        <table className={css.table}>
+            <thead className={css.thead}>
+                <tr>
+                    <th className={css.th}>
+                        Type
+                    </th>
+                    <th className={css.th}>
+                        Amount
+                    </th>
+                    <th className={css.th}>
+                        Currency
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {items.map((item) => {
+                    return (
+                        <tr key={item.id} className={css.tr}>
+                        <td className={css.tdType}>{item.type}</td>
+                        <td className={css.td}>{item.amount}</td>
+                        <td className={css.td}>{item.currency}</td>
+                    </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+        </>
+    )
+}
+
+export default Transaction;
